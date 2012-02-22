3e0bc515679dd8c8d3df416b5b5453d3d034a1e1
David Doria <daviddoria@gmail.com>
2012-02-22 15:46:45 -0500

    Added some new tools.

---
+++ Information.cxx
@@ -0,0 +1,30 @@
+#include "itkVectorImage.h"
+#include "itkImageFileReader.h"
+
+typedef itk::VectorImage<float, 2> ImageType;
+
+int main(int argc, char *argv[])
+{
+  // Verify arguments
+  if(argc < 2)
+    {
+    std::cerr << "Required: inputFilename" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  // Parse arguments
+  std::string inputFilename = argv[1];
+
+  // Output arguments
+  std::cout << "inputFilename " << inputFilename << std::endl;
+
+  // Read file
+  typedef itk::ImageFileReader<ImageType> ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName(inputFilename);
+  reader->Update();
+
+  std::cout << "Number of components: " << reader->GetOutput()->GetNumberOfComponentsPerPixel() << std::endl;
+
+  return EXIT_SUCCESS;
+}
